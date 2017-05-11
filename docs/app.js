2c974749a5f977e507bf52a6a87437e34b5db734
Paul Irish <paul.irish@gmail.com>
2017-05-11 16:29:57 -0700

    clean

--- docs/app.js
+++ docs/app.js
@@ -1,24 +1,5 @@
-
-
-import {$on} from './helpers.js'
 import {updateTodo} from './todo.js'
-// import toggleGraph from './graph/index.js'
 
 export function onLoad() { // eslint-disable-line import/prefer-default-export
   updateTodo()
-  // const toggleGraphButton = document.querySelector('.toggle-graph')
-  // $on(
-  //   toggleGraphButton,
-  //   'click',
-  //   () => {
-  //     const active = toggleGraph()
-  //     if (active) {
-  //       toggleGraphButton.classList.add('active')
-  //     } else {
-  //       toggleGraphButton.classList.remove('active')
-  //     }
-  //   },
-  // )
 }
-
-console.log('sup')
